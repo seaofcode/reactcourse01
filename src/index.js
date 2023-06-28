b955147740c81da955c442649c354ac55ee2ed7c
web-mjp <seaofcode@gmail.com>
2023-06-27 22:25:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return React.createElement('h2', {}, 'hello world');
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
